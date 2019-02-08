0e5f5eb18d80c6fb2f1087e921f7d1f023fe6d3c
shengshengqi <1296813320@qq.com>
2019-02-08 14:14:53 +0800

    搜索功能完成！s

--- view/src/components/search.vue
+++ view/src/components/search.vue
@@ -1,10 +1,10 @@
 <template>
 <div class='cs'>
-  <el-input placeholder="请输入型号" v-model="input5" class="input-with-select">
+  <el-input placeholder="请输入型号" v-model="input" class="input-with-select">
     <el-select v-model="select" slot="prepend" placeholder="请选择">
-      <el-option label="电容" value="1"></el-option>
-      <el-option label="电阻" value="2"></el-option>
-      <el-option label="电感" value="3"></el-option>
+      <el-option label="电容" value="电容"></el-option>
+      <el-option label="电阻" value="电阻"></el-option>
+      <el-option label="电感" value="电感"></el-option>
     </el-select>
     <el-button slot="append" icon="el-icon-search" @click="auth()"></el-button>
   </el-input>
@@ -16,15 +16,19 @@ export default {
   name:'searchlan',
   data() {
     return {
-      input3: '',
-      input4: '',
-      input5: '',
+      input: '',
       select: ''
     }
   },
   methods:{
       async auth(){
-        this.$router.push("/sresistance");
+        this.$router.push({
+          path:"/ssearch",
+          query:{
+            input:this.input,
+            select:this.select
+          }
+        });
       }
   }
 
--- view/src/service/api.js
+++ view/src/service/api.js
@@ -24,10 +24,19 @@ let  Capacitance = async()=> {
     let response = await request(`get`, `${config.Capacitance}`)
     return JSON.parse(JSON.stringify(response))
 }
+//查找
+let Search = async(typeid,pname)=>{
+    let response = await request(`post`,`${config.Search}`,{
+        typeid:typeid,
+        pname:pname
+    })
+    return JSON.parse(JSON.stringify(response))
+}
 
 export default {
     Login:Login,
     Inductance:Inductance,
     Resistance:Resistance,
-    Capacitance:Capacitance
+    Capacitance:Capacitance,
+    Search:Search
 }
